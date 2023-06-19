361747ebb7eacbc136651e4f359f2f3565a3bf98
Kentaro Kuwata <kuwaken0925@gmail.com>
2023-06-19 01:00:42 -0700

    Add SparkSession reader for XGBoost4J

--- src/main/java/data/ReadDataWithSpark.java
+++ src/main/java/data/ReadDataWithSpark.java
@@ -2,16 +2,31 @@ package data;
 
 import java.io.*;
 
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.spark.ml.feature.StringIndexerModel;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+
 import smile.data.type.*;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.parquet.avro.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 
 import java.util.*;
+
+
+import static org.apache.parquet.avro.AvroParquetReader.builder;
 
 
 public class ReadDataWithSpark {
 
 
-    StructType createSchema(String filePath) {
+    List<StructField> createSchema(String filePath) {
 
         List<StructField> fieldList = new ArrayList<>();
 
@@ -32,12 +47,55 @@ public class ReadDataWithSpark {
 
                 fieldList.add(new StructField(column, dataType));
             }
-            return fieldList;
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return fieldList;
+    }
 
+    public ParquetReader<GenericRecord> buildParquetReader(String filePath) throws IOException {
+        Configuration conf = new Configuration();
+
+        conf.setBoolean(AvroReadSupport.AVRO_COMPATIBILITY, true);
+
+        Path input = new Path(filePath);
+
+        return AvroParquetReader
+                .<GenericRecord>builder(HadoopInputFile.fromPath(input, conf))
+                .withConf(conf)
+                .build();
+    }
+
+    private Map<String, Object> recordToMap(GenericRecord record, List<StructField> fieldList) {
+        Map<String, Object> recordMap = new HashMap<>();
+
+        for (StructField field : fieldList) {
+            Object value = (!record.hasField(field.name) || record.get(field.name) == null) ? Double.NaN : record.get(field.name);
+
+            recordMap.put(field.name, value);
+        }
+
+        return recordMap;
+    }
+
+    public Dataset<Row> convertToDMatrix(String schema, String[] inputNames, String csvPath) {
+        SparkSession spark = SparkSession.builder().getOrCreate();
+        Dataset<Row> rawInput = spark.read().schema(schema).csv(csvPath);
+
+        StringIndexerModel stringIndexer = new StringIndexer()
+                .setInputCols(inputNames)
+                .setOutputCol("")
+                .fit(rawInput);
+
+        Dataset<Row> labelTransformed = stringIndexer.transform(rawInput).drop("");
+
+        VectorAssembler vectorAssembler = new VectorAssembler().
+                setInputCols(inputNames).
+                setOutputCol("features");
+
+        return vectorAssembler.transform(labelTransformed).select("features", "target");
     }
 
 }
